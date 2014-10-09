2412d6890c45269d1a94f25164e7da76593bea5a
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-08 20:27:14 -0400

    Splash screen layed out.

--- src/me/userinterface/bookprice/SplashActivity.java
+++ src/me/userinterface/bookprice/SplashActivity.java
@@ -3,10 +3,10 @@ package me.userinterface.bookprice;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class SplashScreen extends Activity {
+public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_scanner);
+		setContentView(R.layout.activity_splash);
 	}
 }
