6c267b47729fa33fd566e00b3d66f7ab88ca176a
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-08 20:34:39 -0400

    Splash activity runnable in place. Delay for 1500 millis.

--- src/me/userinterface/bookprice/SplashActivity.java
+++ src/me/userinterface/bookprice/SplashActivity.java
@@ -1,12 +1,26 @@
 package me.userinterface.bookprice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+
+		new Handler().postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				Intent i = new Intent(SplashActivity.this,
+						ZBarScannerActivity.class);
+				startActivity(i);
+				finish();
+			}
+		}, 1500);
 	}
 }
