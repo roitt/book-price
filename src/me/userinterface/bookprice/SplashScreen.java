b24567b79a105375ca499704d964bd32386f07f2
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-08 20:06:24 -0400

    Spalsh screen activity added.

---
+++ src/me/userinterface/bookprice/SplashScreen.java
@@ -0,0 +1,12 @@
+package me.userinterface.bookprice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_scanner);
+	}
+}
