6c0e9d943e1a1c7b03c4f92f7dcd3be421906367
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-17 14:18:16 -0400

    BookItem bean added.

---
+++ src/me/userinterface/bookprice/utils/BookItem.java
@@ -0,0 +1,50 @@
+package me.userinterface.bookprice.utils;
+
+public class BookItem {
+	private String source;
+	private String url;
+	private String title;
+	private float price;
+	private String thumbnailUrl;
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public String getThumbnailUrl() {
+		return thumbnailUrl;
+	}
+
+	public void setThumbnailUrl(String thumbnailUrl) {
+		this.thumbnailUrl = thumbnailUrl;
+	}
+
+}
