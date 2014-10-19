baf0636539e7fcce195f057fd1dcc439c97b2a38
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-18 22:11:08 -0400

    Moved fetchbookdata method to a new class.

--- src/me/userinterface/bookprice/BookPricesActivity.java
+++ src/me/userinterface/bookprice/BookPricesActivity.java
@@ -1,12 +1,120 @@
 package me.userinterface.bookprice;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import me.userinterface.bookprice.utils.BookItem;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.devspark.appmsg.AppMsg;
 
 public class BookPricesActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_book_price_list);
+
+		Bundle b = getIntent().getExtras();
+		String isbn = "";
+		if (b != null) {
+			isbn = b.getString("isbn");
+		}
+
+		if (isbn != "")
+			new FetchPriceData(isbn).execute();
+	}
+
+	private class FetchPriceData extends AsyncTask<String, Void, String> {
+		ProgressDialog pDialog;
+		String base_address = "http://api.dataweave.in/v1/book_search/searchByIsbn/?";
+		String api_key = "8a6bc2a929d699c8aa8ffabb3e932c22a1c45cee";
+		String api_address = "";
+
+		protected FetchPriceData(String isbn) {
+			api_address = base_address + "api_key=" + api_key + "&isbn=" + isbn;
+		}
+
+		@Override
+		protected void onPreExecute() {
+			pDialog = ProgressDialog.show(BookPricesActivity.this, null,
+					"Please wait while we give you the best prices.", true);
+		}
+
+		protected String doInBackground(String... params) {
+			// TODO Auto-generated method stub
+			Log.v("do InBackground", "doInBackground");
+			HttpClient httpClient = new DefaultHttpClient();
+			HttpResponse response;
+			String responseString = "";
+			try {
+				response = httpClient.execute(new HttpGet(api_address));
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
+				response.getEntity().writeTo(out);
+				out.close();
+				responseString = out.toString();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+				return "";
+			} catch (IOException e) {
+				e.printStackTrace();
+				return "";
+			}
+			return responseString;
+		}
+
+		@Override
+		protected void onPostExecute(String resultJSON) {
+			pDialog.dismiss();
+			if (resultJSON != "") {
+				try {
+					JSONObject mainObject = new JSONObject(resultJSON);
+					if (mainObject != null && mainObject.has("data")) {
+						JSONArray data = mainObject.getJSONArray("data");
+						BookItem bi;
+						for (int i = 0; i < data.length(); i++) {
+							bi = new BookItem();
+							JSONObject obj = data.getJSONObject(i);
+							if (obj != null && obj.has("title")) {
+								bi.setTitle(obj.getString("title"));
+							}
+							if (obj != null && obj.has("url")) {
+								bi.setUrl(obj.getString("url"));
+							}
+							if (obj != null && obj.has("source")) {
+								bi.setSource(obj.getString("source"));
+							}
+							if (obj != null && obj.has("price")) {
+								bi.setPrice((float) obj.getDouble("price"));
+							}
+							if (obj != null && obj.has("thumbnail")) {
+								bi.setThumbnailUrl(obj.getString("thumbnail"));
+							}
+						}
+					}
+				} catch (JSONException ex) {
+					ex.printStackTrace();
+				}
+
+			} else {
+				AppMsg.makeText(
+						BookPricesActivity.this,
+						"Sorry, could not fetch prices at the moment. Please try again later.",
+						AppMsg.STYLE_ALERT).show();
+			}
+		}
 	}
 }
--- src/me/userinterface/bookprice/ShowBookActivity.java
+++ src/me/userinterface/bookprice/ShowBookActivity.java
@@ -1,29 +1,13 @@
 package me.userinterface.bookprice;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.devspark.appmsg.AppMsg;
 import com.squareup.picasso.Picasso;
 
 public class ShowBookActivity extends Activity {
@@ -84,71 +68,7 @@ public class ShowBookActivity extends Activity {
 						BookPricesActivity.class);
 				i.putExtra("isbn", isbn10);
 				startActivity(i);
-				// new FetchPriceData(isbn10).execute();
 			}
 		});
-	}
-
-	private class FetchPriceData extends AsyncTask<String, Void, String> {
-		ProgressDialog pDialog;
-		String base_address = "http://api.dataweave.in/v1/book_search/searchByIsbn/?";
-		String api_key = "8a6bc2a929d699c8aa8ffabb3e932c22a1c45cee";
-		String api_address = "";
-
-		protected FetchPriceData(String isbn) {
-			api_address = base_address + "api_key=" + api_key + "&isbn=" + isbn;
-		}
-
-		@Override
-		protected void onPreExecute() {
-			pDialog = ProgressDialog.show(ShowBookActivity.this, null,
-					"Please wait while we give you the best prices.", true);
-		}
-
-		protected String doInBackground(String... params) {
-			// TODO Auto-generated method stub
-			Log.v("do InBackground", "doInBackground");
-			HttpClient httpClient = new DefaultHttpClient();
-			HttpResponse response;
-			String responseString = "";
-			try {
-				response = httpClient.execute(new HttpGet(api_address));
-				ByteArrayOutputStream out = new ByteArrayOutputStream();
-				response.getEntity().writeTo(out);
-				out.close();
-				responseString = out.toString();
-			} catch (ClientProtocolException e) {
-				e.printStackTrace();
-				return "";
-			} catch (IOException e) {
-				e.printStackTrace();
-				return "";
-			}
-			return responseString;
-		}
-
-		@Override
-		protected void onPostExecute(String resultJSON) {
-			pDialog.dismiss();
-			if (resultJSON != "") {
-				try {
-					JSONObject mainObject = new JSONObject(resultJSON);
-					if (mainObject != null && mainObject.has("data")) {
-						JSONArray data = mainObject.getJSONArray("data");
-						for (int i = 0; i < data.length(); i++) {
-
-						}
-					}
-				} catch (JSONException ex) {
-					ex.printStackTrace();
-				}
-
-			} else {
-				AppMsg.makeText(
-						ShowBookActivity.this,
-						"Sorry, could not fetch prices at the moment. Please try again later.",
-						AppMsg.STYLE_ALERT).show();
-			}
-		}
 	}
 }
