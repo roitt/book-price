1f4ccde95f18d2114bd984887dfd88f8c71bb826
Rohit Bhoompally <bhoomprt@mail.uc.edu>
2014-10-18 21:49:27 -0400

    Book price list activity added.

---
+++ src/me/userinterface/bookprice/BookPricesActivity.java
@@ -0,0 +1,12 @@
+package me.userinterface.bookprice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BookPricesActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_book_price_list);
+	}
+}
